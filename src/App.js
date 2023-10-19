e39f957506bd113e20890f748c886b7b245b7500
omaah2 <ummkhaladoluwatosin@gmail.com>
2023-10-19 14:38:24 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Home from "./components/Home";
 import RepoList from "./components/RepoList";
@@ -10,11 +10,33 @@ function App() {
   return (
     <Router>
       <ErrorBoundary>
+        <nav className="flex align-items-center bg-purple-400 p-4 text-white">
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/non-existent-route" className="hover:underline">
+                Test 404 Page
+              </Link>
+            </li>
+            <li>
+              <Link to="/errorBoundary" className="hover:underline">
+                Test ErrorBoundary
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/repos" element={<RepoList />} />
           <Route path="/repos/:repoName" element={<RepoDetail />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/errorBoundary" element={<ErrorBoundary />} />
         </Routes>
       </ErrorBoundary>
     </Router>
